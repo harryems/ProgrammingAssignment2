315fd93f01281fc30c9377ddf623f8d1ebca6f57
harryems <carlos.espinosa@outlook.com>
2014-05-24 20:22:56 -0400

    Cache working

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+setClass("cachemean",representation(set = "function",get = "function", getmean = "function",setmean = "function"))
 
+makeVector <- function(x) {
+        data <- x
+        datamean <- NULL
+        get <- function() data
+        set <- function(newvalue) {
+                data <<- newvalue
+                datamean <<- NULL
+        }
+        setmean <- function(newmean) datamean <<- newmean
+        getmean <- function() datamean
+        new("cachemean", set = set, get = get, setmean = setmean,
+            getmean = getmean)
 }
 
+setMethod("show", "cachemean",
+          function(object) {
+                  x <- object$get()
+                  show(x)
+                  invisible(x)
+          })
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+setMethod("mean", "cachemean",
+          function(x, ...) {
+                  m <- x$getmean()
+                  if(!is.null(m))
+                          return(m)
+                  data <- x$get()
+                  m <- mean(data, ...)
+                  x$setmean(m)
+                  m
+          })
